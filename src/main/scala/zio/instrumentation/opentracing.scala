d6722fac19ef45aeb5ec2da9c652c4044d03951c
Tamer Abdulradi <tamer@abdulradi.com>
2019-05-31 02:28:42 +0100

    Seperate propagation concerns from span creation/logging/etc

--- src/main/scala/zio/instrumentation/opentracing.scala
+++ src/main/scala/zio/instrumentation/opentracing.scala
@@ -17,87 +17,105 @@ object TracingValue {
   implicit def tracingValueFromBigDecimal(value: BigDecimal): TracingValue = TracingValue.NumberTracingValue(value)
 }
 
+/** Pure version of opentracing Tracer and Span operations */
+object Tracer {
+  type Headers = Map[String, String]
+}
+trait Tracer[Span] {
+  import Tracer.Headers
+  def startSpan(operationName: String, upstreamSpan: Headers): IO[Throwable, Span]
+  def export(span: Span): Headers
+  def startChild(span: Span, operationName: String): IO[Throwable, Span]
+  def finish(span: Span): UIO[Unit]
+  def setTag(span: Span, key: String, value: TracingValue): IO[Throwable, Unit]
+  def log(span: Span, fields: Seq[(String, TracingValue)]): IO[Throwable, Unit]
+  def setBaggageItem(span: Span, key: String, value: String): IO[Throwable, Unit]
+  def getBaggageItem(span: Span, key: String): IO[Throwable, Option[String]]
+}
+
+class OpenTracingTracer(tracer: io.opentracing.Tracer) extends Tracer[io.opentracing.Span] {
+  import Tracer.Headers
+  import io.opentracing.propagation._
+  import io.opentracing.Span
+  import scala.collection.JavaConverters._
+
+  override def startSpan(operationName: String, upstreamSpan: Headers): IO[Throwable, Span] = {
+    val upstream = tracer.extract(Format.Builtin.HTTP_HEADERS, new TextMapExtractAdapter(upstreamSpan.asJava))
+    val span =
+      tracer
+        .buildSpan(operationName)
+        .asChildOf(upstream)
+    // TODO: Double check that the java code above doesn't side effect
+    ZIO(span.start)
+  }
+
+  override def export(span: Span): Headers = {
+    val carrier = new java.util.HashMap[String, String]() // Warning, mutability ahead!
+    tracer.inject(span.context, Format.Builtin.HTTP_HEADERS, new TextMapInjectAdapter(carrier))
+    carrier.asScala.toMap
+  }
+
+  override def startChild(span: Span, operationName: String): IO[Throwable, Span] =
+    ZIO(tracer.buildSpan(operationName).asChildOf(span).start())
+  override def finish(span: Span): UIO[Unit] = UIO(span.finish())
+  override def setTag(span: Span, key: String, v: TracingValue): IO[Throwable, Unit] = v match {
+    case TracingValue.StringTracingValue(value)  => ZIO(span.setTag(key, value)).unit
+    case TracingValue.NumberTracingValue(value)  => ZIO(span.setTag(key, value)).unit
+    case TracingValue.BooleanTracingValue(value) => ZIO(span.setTag(key, value)).unit
+  }
+  override def log(span: Span, fields: Seq[(String, TracingValue)]): IO[Throwable, Unit] = {
+    val jFields = new java.util.HashMap[String, Any]()
+    fields.foreach {
+      case (key, TracingValue.StringTracingValue(value))  => jFields.put(key, value)
+      case (key, TracingValue.NumberTracingValue(value))  => jFields.put(key, value)
+      case (key, TracingValue.BooleanTracingValue(value)) => jFields.put(key, value)
+    }
+    ZIO(span.log(jFields)).unit
+  }
+
+  override def setBaggageItem(span: Span, key: String, value: String): IO[Throwable, Unit] =
+    ZIO(span.setBaggageItem(key, value)).unit
+  override def getBaggageItem(span: Span, key: String): IO[Throwable, Option[String]] =
+    ZIO(Option(span.getBaggageItem(key)))
+
+}
+
+/** Deals with current span propagation */
 trait Tracing extends Serializable {
-  val tracing: Tracing.Service
+  def tracing: Tracing.Service
 }
 object Tracing extends Serializable {
-  type Headers = Map[String, String]
-
   trait Service {
-    type Span
-
-    def currentSpan: FiberLocal[Span]
-    final def inAChildSpan[R <: Console, E, A](
+    def inAChildSpan[R <: Console, E, A](
       use: ZIO[R, E, A]
-    )(operationName: String, tags: Seq[(String, TracingValue)]): ZIO[R, Any, A] = { // FIXME: Error is Any
-      val acquire = for {
-        parent <- currentSpan.get.get
-        span   <- startChild(parent, operationName)
-        _      <- ZIO.foreach(tags) { case (k, v) => setTag(span, k, v) }
-      } yield (finish(span), currentSpan.locally(span)(use).fork)
+    )(operationName: String, tags: Seq[(String, TracingValue)]): ZIO[R, Any, A]
 
-      ZIO.bracket(acquire)(_._1)(_._2.flatMap(_.join)) // TODO: FiberLocal.local ?
-    }
-
-    /** Selected functionality from io.opentracing.{Tracer, Span} */
-    def startSpan(operationName: String, upstreamSpan: Headers): IO[Throwable, Span]
-    def export(span: Span): Headers
-    def startChild(span: Span, operationName: String): IO[Throwable, Span]
-    def finish(span: Span): UIO[Unit]
-    def setTag(span: Span, key: String, value: TracingValue): IO[Throwable, Unit]
-    def log(span: Span, fields: Seq[(String, TracingValue)]): IO[Throwable, Unit]
-    def setBaggageItem(span: Span, key: String, value: String): IO[Throwable, Unit]
-    def getBaggageItem(span: Span, key: String): IO[Throwable, Option[String]]
+    def log(fields: (String, TracingValue)*): IO[Any, Unit]
   }
-}
 
-class OpenTracing(tracer: io.opentracing.Tracer, currentFiberLocal: FiberLocal[io.opentracing.Span]) extends Tracing {
-  import Tracing.Headers
-  import io.opentracing.propagation._
-  import scala.collection.JavaConverters._
-  override val tracing: Tracing.Service = new Tracing.Service {
-    override type Span = io.opentracing.Span
+  // FIXME: Won't work, instantiation has to happen at the edge of the world, to be mixed-in with rest of dependencies
+  // def make[Span](tracer: Tracer[Span]): UIO[Tracing] =
+  //   for {
+  //     currentSpan <- FiberLocal.make[Span]
+  //   } yield new Live(tracer, currentSpan)
 
-    override def currentSpan: FiberLocal[Span] = currentFiberLocal
+  class Live[Span](tracer: Tracer[Span], val currentSpan: FiberLocal[Span]) extends Tracing {
+    override val tracing: Tracing.Service = new Tracing.Service {
+      override def inAChildSpan[R <: Console, E, A](
+        use: ZIO[R, E, A]
+      )(operationName: String, tags: Seq[(String, TracingValue)]): ZIO[R, Any, A] = { // FIXME: Error is Any
+        val acquire = for {
+          parent <- currentSpan.get.get
+          span   <- tracer.startChild(parent, operationName)
+          _      <- ZIO.foreach(tags) { case (k, v) => tracer.setTag(span, k, v) }
+        } yield (tracer.finish(span), currentSpan.locally(span)(use).fork)
 
-    override def startSpan(operationName: String, upstreamSpan: Headers): IO[Throwable, Span] = {
-      val upstream = tracer.extract(Format.Builtin.HTTP_HEADERS, new TextMapExtractAdapter(upstreamSpan.asJava))
-      val span =
-        tracer
-          .buildSpan(operationName)
-          .asChildOf(upstream)
-      // TODO: Double check that the java code above doesn't side effect
-      ZIO(span.start)
-    }
-
-    override def export(span: Span): Headers = {
-      val carrier = new java.util.HashMap[String, String]() // Warning, mutability ahead!
-      tracer.inject(span.context, Format.Builtin.HTTP_HEADERS, new TextMapInjectAdapter(carrier))
-      carrier.asScala.toMap
-    }
-
-    override def startChild(span: Span, operationName: String): IO[Throwable, Span] =
-      ZIO(tracer.buildSpan(operationName).asChildOf(span).start())
-    override def finish(span: Span): UIO[Unit] = UIO(span.finish())
-    override def setTag(span: Span, key: String, v: TracingValue): IO[Throwable, Unit] = v match {
-      case TracingValue.StringTracingValue(value)  => ZIO(span.setTag(key, value)).unit
-      case TracingValue.NumberTracingValue(value)  => ZIO(span.setTag(key, value)).unit
-      case TracingValue.BooleanTracingValue(value) => ZIO(span.setTag(key, value)).unit
-    }
-    override def log(span: Span, fields: Seq[(String, TracingValue)]): IO[Throwable, Unit] = {
-      val jFields = new java.util.HashMap[String, Any]()
-      fields.foreach {
-        case (key, TracingValue.StringTracingValue(value))  => jFields.put(key, value)
-        case (key, TracingValue.NumberTracingValue(value))  => jFields.put(key, value)
-        case (key, TracingValue.BooleanTracingValue(value)) => jFields.put(key, value)
+        ZIO.bracket(acquire)(_._1)(_._2.flatMap(_.join))
       }
-      ZIO(span.log(jFields)).unit
-    }
 
-    override def setBaggageItem(span: Span, key: String, value: String): IO[Throwable, Unit] =
-      ZIO(span.setBaggageItem(key, value)).unit
-    override def getBaggageItem(span: Span, key: String): IO[Throwable, Option[String]] =
-      ZIO(Option(span.getBaggageItem(key)))
+      override def log(fields: (String, TracingValue)*) =
+        currentSpan.get.get.flatMap(tracer.log(_, fields))
+    }
   }
 }
 
@@ -107,13 +125,7 @@ object TracingZioApi {
       ZIO.access[Tracing](_.tracing).flatMap(_.inAChildSpan(self)(operationName, tags))
   }
 
-  def log(fields: (String, TracingValue)*) = ZIO.access[Tracing] { r =>
-    for {
-      span <- r.tracing.currentSpan.get
-      _    <- r.tracing.log(span.get, fields)
-    } yield ()
-  }
-
+  def log(fields: (String, TracingValue)*) = ZIO.access[Tracing](_.tracing.log(fields: _*))
 }
 
 object MyApp extends App {
@@ -125,15 +137,14 @@ object MyApp extends App {
       .withSampler(SamplerConfiguration.fromEnv().withType("const").withParam(1))
       .withReporter(ReporterConfiguration.fromEnv().withLogSpans(true))
 
-  def dirtyLog[A](a: A): A = { println(a); a }
-
+  // Fixme: Instantiation is a bit cumbersome 
   def run(args: List[String]) =
     for {
-      currentSpan <- FiberLocal.make[io.opentracing.Span] // to be created by http middleware for every request
+      currentSpan <- FiberLocal.make[io.opentracing.Span]
       tracer      <- UIO(tracerConf.getTracer)
-      r           = new OpenTracing(tracer, currentSpan) with Console.Live
+      r           = new Tracing.Live(new OpenTracingTracer(tracer), currentSpan) with Console.Live
       rootSpan    <- UIO(tracer.buildSpan("REQUEST-ROOT").start())
-      res         <- currentSpan.locally(rootSpan)(myAppLogic.fold(x => { dirtyLog(x); 1 }, _ => 0).provide(r))
+      res         <- currentSpan.locally(rootSpan)(myAppLogic.fold(_ => 1, _ => 0).provide(r))
       _           <- UIO(rootSpan.finish())
     } yield res
 
@@ -147,9 +158,7 @@ object MyApp extends App {
   def doSomething(n: Int) =
     for {
       _ <- putStrLn(s"Child 1 of Parent $n").instrumented(s"child1-of-parent$n", "child" -> 1, "parent"  -> n)
-      _ <- log("LEVEL" -> "DEBUG", 
-               "n" -> n, 
-               "isFoo" -> true)
+      _ <- log("LEVEL" -> "DEBUG", "n" -> n, "isFoo" -> true)
       _ <- putStrLn(s"Child 2 of Parent $n").instrumented(s"child2-of-parent$n", "child" -> 2, "parent"  -> n)
     } yield ()
 }
